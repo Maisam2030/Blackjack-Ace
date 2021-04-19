4573f85d90b28e7ea2500cb2786e41ee3e03272f
Maisam Adalati <adalatim@sheridancollege.ca>
2021-04-19 17:17:07 -0400

    1st commit

---
+++ test/ca/sheridancollege/project/CardIT.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author imbes
+ */
+public class CardIT {
+    
+    public CardIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of toString method, of class Card.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        Card instance = new CardImpl();
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    public class CardImpl extends Card {
+
+        public String toString() {
+            return "";
+        }
+    }
+    
+}
