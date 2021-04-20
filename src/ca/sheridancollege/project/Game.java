e8f5fe94fef15d69b4c570d7f66c06d0a4faed3e
Maisam Adalati <adalatim@sheridancollege.ca>
2021-04-19 22:32:12 -0400

    1st commit

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -5,54 +5,141 @@
  */
 package ca.sheridancollege.project;
 
-import java.util.ArrayList;
+import java.util.Scanner; 
 
-/**
- * The class that models your game. You should create a more specific child of this class and instantiate the methods
- * given.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
- */
-public abstract class Game {
-
-    private final String name;//the title of the game
-    private ArrayList<Player> players;// the players of the game
-
-    public Game(String name) {
-        this.name = name;
-        players = new ArrayList();
+public class Game {
+    
+    public static int autoGenerate(int randomCards) { 
+        
+        int maxedOut = 6; 
+        
+        int minedOut = 1; 
+        
+        int result = (maxedOut - minedOut)+1; 
+       
+        
+         randomCards = (int)(Math.random()*result);
+         
+         if (randomCards == 0) {
+             randomCards = 1; 
+         }
+          
+          return randomCards; 
+    } 
+    
+    public static void main(String[] args) {
+          Scanner theUser = new Scanner(System.in); 
+        int randomCards = 0; 
+        int add = +autoGenerate(randomCards); 
+          System.out.println(+add);
+        System.out.println("Continue? You currently have: "+add);
+          String userAnswer = theUser.nextLine();
+          
+           if (userAnswer.equals("yes")) {
+              int add2 = +autoGenerate(randomCards); 
+              int total = add+add2; 
+              if (total < 21) {
+               System.out.println("Continue? You currently have: "+total);
+                userAnswer = theUser.nextLine();
+           
+             if (userAnswer.equals("yes")) {
+              int add3 = +autoGenerate(randomCards); 
+               int total2 = add+add2+add3; 
+                if (total2 < 21) {
+                System.out.println("Continue? You currently have: "+total2);
+                           userAnswer = theUser.nextLine();
+                }
+                
+                else if (total2 > 21) {
+               System.out.println("You lose! Your Value: "+add+add2+add3);
+                }
+                
+                 if (userAnswer.equals("yes")) {
+              int add4 = +autoGenerate(randomCards); 
+               int total3 = add+add2+add3+add4; 
+                if (total3 < 21) {
+                System.out.println("Continue? You currently have: "+total3);
+                           userAnswer = theUser.nextLine();
+                }
+                
+                else if (total3 > 21) {
+               System.out.println("You lose! Your Value: "+add+add2+add3+add4);
+                }
+                 
+                  if (userAnswer.equals("yes")) {
+              int add5 = +autoGenerate(randomCards); 
+               int total4 = add+add2+add3+add4+add5; 
+                if (total4 < 21) {
+                System.out.println("Continue? You currently have: "+total4);
+                           userAnswer = theUser.nextLine();
+                }
+                
+                else if (total4 > 21) {
+               System.out.println("You lose! Your Value: "+add+add2+add3+add4);
+                }
+                 
+                  if (userAnswer.equals("yes")) {
+              int add6 = +autoGenerate(randomCards); 
+               int total5 = add+add2+add3+add4+add5+add6; 
+                if (total5 < 21) {
+                System.out.println("Continue? You currently have: "+total5);
+                           userAnswer = theUser.nextLine();
+                           
+                }
+                else if (total5 > 21) {
+               System.out.println("You lose! Your Value: "+add+add2+add3+add4+add5);
+                }
+                 
+                 if (userAnswer.equals("yes")) {
+              int add7 = +autoGenerate(randomCards); 
+               int total6 = add+add2+add3+add4+add5+add6+add7; 
+                if (total6 < 21) {
+                System.out.println("Continue? You currently have: "+total6);
+                           userAnswer = theUser.nextLine();
+                }
+                
+                else if (total6 > 21) {
+               System.out.println("You lose! Your Value: "+add+add2+add3+add4+add5+add6+add7);
+                }
+                
+                if (userAnswer.equals("yes")) {
+              int add8 = +autoGenerate(randomCards); 
+               int total7 = add+add2+add3+add4+add5+add6+add7+add8; 
+                if (total7 < 21) {
+                System.out.println("Continue? You currently have: "+total7);
+                           userAnswer = theUser.nextLine();
+                }
+                
+                else if (total7 > 21) {
+               System.out.println("You lose! Your Value: "+total7);
+                }
+                 
+                 if (userAnswer.equals("yes")) {
+              int add9 = +autoGenerate(randomCards); 
+               int total8 = add+add2+add3+add4+add5+add6+add7+add8+add9; 
+                if (total8 < 21) {
+                System.out.println("Continue? You currently have: "+total8);
+                           userAnswer = theUser.nextLine();
+                }
+                
+                else if (total8 > 21) {
+               System.out.println("You lose! Your Value: "+total8);
+                }
+                 
+             }
+             }
+           }
+             
+             
+             else {
+               System.out.println("Thank you For Playing!");
+       }
+     
+       }
     }
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @return the players of this game
-     */
-    public ArrayList<Player> getPlayers() {
-        return players;
-    }
-
-    /**
-     * @param players the players of this game
-     */
-    public void setPlayers(ArrayList<Player> players) {
-        this.players = players;
-    }
-
-    /**
-     * Play the game. This might be one method or many method calls depending on your game.
-     */
-    public abstract void play();
-
-    /**
-     * When the game is over, use this method to declare and display a winning player.
-     */
-    public abstract void declareWinner();
-
-}//end class
+}
+}
+}
+}
+}
+}
